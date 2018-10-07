be9885004b4ed55b4eac27e2165db3b451c1883c
Sethu Senthil <sethunarayansenthil@gmail.com>
2018-10-07 13:05:19 -0400

    Added Docs and bug fixes

--- README.md
+++ README.md
@@ -1,1 +1,50 @@
-# Lil-query
+# Lil-query (BETA)
+Write Less, Do a lil bit more. \
+![alt text](./images/bannerLogo.png "Banner Logo")
+###### Image by [Vishnu Kumar](https://www.instagram.com/vshnukmr/)
+
+Lil Query is a minimalist js library for getting $#*! for the DOM. By shorting Javascript syntax, without compromising in readability, it's a very convenient tool to add into your development cycle, and you will hate to go back.
+
+# Getting Started
+Download the js files here \
+or use the raw git \
+\
+JS fiddle boilerplate
+
+# Getting an element (from the DOM)
+Getting element from the DOM shouldn't be as long or as hard as it is right now. Instead you can use the get function and pass in a string with the proper id class speficier.
+`get('#id')` \
+When getting element by class it is required to pass in a function in string format as the second peremater, unless the class contains only one element. You can reffer the elements under the class name by using 'el'. \
+`let theFunction = () =>{el.style.color = 'blue'};` \
+`get('.class','theFunction')`
+
+# Getting an element by Id
+id - type locked id selector/
+`id('elementID).style.color = 'blue'` \
+To be consistent you may enter a function (in string format) as the second peremater just like before. /
+`let theFunction = () =>{el.style.color = 'blue'};` \
+`id('elementID','theFunction')`
+
+# Getting element by class
+classGetAll -  type locked class selector \
+`let theFunction = () =>{el.style.color = 'blue'};` \
+`classGetAll('theClass','theFunction')` \
+\
+classGet - gets a certian element from class \
+`classGet('theClass',2).style.color = blue;` \
+\
+classLength - returns the length of a certian class \
+`classLength('theClass')` \
+# Getting element by tag name
+tag - type locked class tag selector
+`tag('tagname', 0)`
+# Accessing the document object
+doc - docment object reference shortcut
+`doc.property`
+
+# Logging to console
+log - console.log shortcut
+`log('hello world')`
+
+# Contributing
+Yes please! Make sure to update the docs, and the TODO list and a minified version of the library.
--- lil-query.js
+++ lil-query.js
@@ -1,25 +1,46 @@
   /*!
-   * lil-query v1.0 (link)
+   * lil-query v0.1 beta(link)
    * Copyright 2018 Sethu Senthil
    * MIT License (link)
    */
   let doc = document,
-   id = (e) => {
-          return doc.getElementById(e);
-      },
-      classGet = (e, n) => {
-          return doc.getElementsByClassName(e)[n];
-      },
-      classLength = (e) => {
-          return doc.getElementsByClassName(e).length;
-      },
-      tag = (e, n) => {
-          return doc.getElementsByTagName(e)[n];
-      },
-      classGetAll = (e, f) => {
-          for (var i = 0; i <= classLength(e) - 1; i++) {
-              ef = `${f}()`;
-              classElem = classGet(e, i);
-              eval(ef);
-          }
-      };
+  el, //exposes element to global
+      log = (se) =>{
+          console.log(se);
+      }
+     id = (e,f) => {
+            el = doc.getElementById(e);
+            if (f !== '') {
+                ef = `${f}();`; //builds function reference into executubile js format
+                eval(ef);
+            }else{
+                return el;
+            }
+        },
+        classGet = (e, n) => {
+            return doc.getElementsByClassName(e)[n];
+        },
+        classLength = (e) => {
+            return doc.getElementsByClassName(e).length;
+        },
+        tag = (e, n) => {
+            return doc.getElementsByTagName(e)[n];
+        },
+        classGetAll = (e, f) => {
+            for (let i = 0; i <classLength(e); i++) {
+         el = classGet(e, i); //fetches current element refferfc in loop iteration
+          ef = `${f}();`; //builds function reference into executubile js format
+          eval(ef);
+            }
+        },
+        get = (e,nf) =>{
+          let fl = e.charAt(0), //Gets first character of the first perameter
+          fc = e.slice(1, e.length); //"Removal" of the first character
+            if (fl === '#') {  //Checks if it's an id, class, or a "getClassAll"
+              return id(fc);
+            }else if(fl === '.' && nf !== ''){
+              return classGet(fc, nf);
+            } else if(fl === '.' && nf === ''){
+               classGetAll(fc, nf)
+            }
+        }
